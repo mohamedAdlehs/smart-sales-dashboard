0fce801ca68b5ffe833ddbe8f4e03903c4150e93
Mohamed Adel <your-email@example.com>
2026-04-08 10:56:41 +0300

    feat: implement app routing and navigation links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Dashboard } from './features/dashboard/dashboard';
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // توجيه الصفحة الرئيسية للـ Dashboard
+  { path: 'dashboard', component: Dashboard}
+  // هنا سنضيف لاحقاً مسارات المنتجات والمبيعات
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+ import { RouterOutlet } from '@angular/router';
 import { Dashboard } from "./features/dashboard/dashboard";
 import { Navbar } from "./shared/components/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard, Navbar],
+  imports: [ Navbar,RouterOutlet,Dashboard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('smart-sales-dashboard');
